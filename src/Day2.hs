e3c944e6eb81b0103657adddb9f18c55a3a0a492
Alessandro Candolini <alessandrocandolini@gmail.com>
2025-12-03 00:16:18 +0000

    day 2 part 1

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,11 +1,40 @@
 module Day2 where
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Text.Megaparsec (Parsec, ParseErrorBundle, sepBy, runParser)
+import Data.Void (Void)
+import Text.Megaparsec.Char.Lexer (decimal)
+import Text.Megaparsec.Char (char)
 
-data Answer = Answer deriving (Eq, Show)
+data Answer = Answer Int deriving (Eq, Show)
 
 program :: T.Text -> IO ()
-program = print . logic
+program = print . solve
 
-logic :: T.Text -> Answer
-logic = const Answer
+solve :: T.Text -> Either ParsingError Answer
+solve = fmap (Answer . solvePart1) . parse
+
+data Range a = Range { from :: a , to :: a } deriving (Eq, Show)
+
+range :: Enum a => Range a -> [a]
+range r = [from r .. to r]
+
+isRepeatedTwice :: Int -> Bool
+isRepeatedTwice = areTheSame . splitInHalf . show where
+  splitInHalf as = splitAt (length as `div` 2) as
+  areTheSame (s1, s2) = s1 == s2
+
+solvePart1 :: [Range Int] -> Int
+solvePart1 = sum . filter isRepeatedTwice . concatMap range
+
+type Parser = Parsec Void T.Text
+type ParsingError = ParseErrorBundle T.Text Void
+
+rangeParser :: Parser (Range Int)
+rangeParser = Range <$> (decimal <* char '-') <*> decimal
+
+rangesParser :: Parser [Range Int]
+rangesParser = sepBy rangeParser (char ',')
+
+parse :: T.Text -> Either ParsingError [Range Int]
+parse = runParser rangesParser "input day 2"
--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -1,21 +1,41 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day2Spec where
 
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Day2
+import NeatInterpolation (trimming)
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
+
+input =
+  [trimming|
+  11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
+|]
+
+ranges :: [Range Int]
+ranges =
+  [ Range 11 22
+  , Range 95 115
+  , Range 998 1012
+  , Range 1188511880 1188511890
+  , Range 222220 222224
+  , Range 1698522 1698528
+  , Range 446443 446449
+  , Range 38593856 38593862
+  , Range 565653 565659
+  , Range 824824821 824824827
+  , Range 2121212118 2121212124
+  ]
 
 spec :: Spec
 spec = describe "Day 2" $ do
-  it ""
-    $ 1
-    `shouldBe` 1
+  it "can parse input" $
+    parse input `shouldBe` Right ranges
+  it "can generate all intermediate numbers" $
+    range (Range 998 1012) `shouldBe` [998, 999, 1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012]
 
-  prop ""
-    $ \l -> reverse (reverse l) == (l :: [Int])
-
-  xit "solve the puzzle" $ do
-     input <- T.readFile "resources/input2"
-     logic input `shouldBe` Answer
+  it "can solve part1" $
+    solvePart1 ranges `shouldBe` 1227775554
