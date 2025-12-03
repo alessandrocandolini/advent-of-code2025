274592804f68317e221d29c5e9b7ba861b33660b
Alessandro Candolini <alessandrocandolini@gmail.com>
2025-12-03 22:15:23 +0000

    Day 3 part 1

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,11 +1,102 @@
-module Day3 where
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
+{-# LANGUAGE DerivingVia #-}
 
-data Answer = Answer deriving (Eq, Show)
+module Day3 where
+
+import Control.Applicative (Alternative (some), (<|>))
+
+-- import Control.Monad.Combinators.NonEmpty (some)
+import Data.Functor (($>))
+import Data.List (group, sortOn, tails)
+import Data.Semigroup
+import qualified Data.Text as T
+import Data.Void (Void)
+import Safe (headMay)
+import Text.Megaparsec (ParseErrorBundle, Parsec, runParser, sepEndBy)
+import Text.Megaparsec.Char
+import Witherable (mapMaybe)
+
+data Answer = Answer Joltage deriving (Eq, Show)
 
 program :: T.Text -> IO ()
-program = print . logic
+program = print . solve
 
-logic :: T.Text -> Answer
-logic = const Answer
+solve :: T.Text -> Either ParsingError Answer
+solve = fmap (Answer . solvePart1) . parse
+
+newtype Joltage = Joltage Int
+  deriving (Eq, Show, Ord, Enum, Bounded, Num) via (Int)
+  deriving (Semigroup, Monoid) via (Sum Int)
+
+data Digit = One | Two | Three | Four | Five | Six | Seven | Eight | Nine deriving (Eq)
+
+digit :: Digit -> Int
+digit One = 1
+digit Two = 2
+digit Three = 3
+digit Four = 4
+digit Five = 5
+digit Six = 6
+digit Seven = 7
+digit Eight = 8
+digit Nine = 9
+
+instance Show Digit where
+  show = show . digit
+
+instance Ord Digit where
+  compare a b = compare (digit a) (digit b)
+
+newtype Battery = Battery {battery :: Digit} deriving (Eq, Show, Ord) via Digit
+
+joltage :: Battery -> Battery -> Joltage
+joltage (Battery b1) (Battery b2) = Joltage $ read $ (show b1) ++ (show b2)
+
+data PowerBank = PowerBank {batteries :: [Battery]} deriving (Eq, Show)
+
+combinations :: [a] -> [(a, a)]
+combinations = concatMap zipWithTail . tails
+ where
+  zipWithTail [] = []
+  zipWithTail (a : as) = (,) <$> [a] <*> as
+
+uniqueCombinations :: (Eq a) => [a] -> [(a, a)]
+uniqueCombinations = combinations . duplicateIfSingleton . mapMaybe headMay . group
+ where
+  duplicateIfSingleton (a : []) = a : [a]
+  duplicateIfSingleton as = as
+
+highestJoltageBatteryPair :: PowerBank -> Maybe (Battery, Battery)
+highestJoltageBatteryPair = headMay . sortOn (negate . uncurry joltage) . uniqueCombinations . batteries
+
+highestJoltage :: PowerBank -> Joltage
+highestJoltage = foldMap (uncurry joltage) . highestJoltageBatteryPair
+
+solvePart1 :: [PowerBank] -> Joltage
+solvePart1 = foldMap highestJoltage
+
+type Parser = Parsec Void T.Text
+type ParsingError = ParseErrorBundle T.Text Void
+
+digitParser :: Parser Digit
+digitParser =
+  char '1' $> One
+    <|> char '2' $> Two
+    <|> char '3' $> Three
+    <|> char '4' $> Four
+    <|> char '5' $> Five
+    <|> char '6' $> Six
+    <|> char '7' $> Seven
+    <|> char '8' $> Eight
+    <|> char '9' $> Nine
+
+batteryParser :: Parser Battery
+batteryParser = fmap Battery digitParser
+
+powerBankParser :: Parser PowerBank
+powerBankParser = fmap PowerBank (some batteryParser)
+
+powerBanksParser :: Parser [PowerBank]
+powerBanksParser = sepEndBy powerBankParser newline
+
+parse :: T.Text -> Either ParsingError [PowerBank]
+parse = runParser powerBanksParser "input day 3"
--- test/Day3Spec.hs
+++ test/Day3Spec.hs
@@ -1,21 +1,85 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day3Spec where
 
+import Data.List.NonEmpty
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Day3
+import Day3 (highestJoltageBatteryPair)
+import NeatInterpolation (trimming)
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
+
+input :: T.Text
+input =
+  [trimming|
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+|]
+
+batteries1 :: [Battery]
+batteries1 = fmap Battery [Nine, Eight, Seven, Six, Five, Four, Three, Two, One, One, One, One, One, One, One]
+
+batteries2 :: [Battery]
+batteries2 = fmap Battery [Eight, One, One, One, One, One, One, One, One, One, One, One, One, One, Nine]
+
+batteries3 :: [Battery]
+batteries3 = fmap Battery [Two, Three, Four, Two, Three, Four, Two, Three, Four, Two, Three, Four, Two, Seven, Eight]
+
+batteries4 :: [Battery]
+batteries4 = fmap Battery [Eight, One, Eight, One, Eight, One, Nine, One, One, One, One, Two, One, One, One]
+
+powerBank1 = PowerBank batteries1
+powerBank2 = PowerBank batteries2
+powerBank3 = PowerBank batteries3
+powerBank4 = PowerBank batteries4
+
+powerBanks :: [PowerBank]
+powerBanks = [powerBank1, powerBank2, powerBank3, powerBank4]
 
 spec :: Spec
 spec = describe "Day 3" $ do
-  it ""
-    $ 1
-    `shouldBe` 1
+  it "parse input" $
+    parse input `shouldBe` (Right powerBanks)
 
-  prop ""
-    $ \l -> reverse (reverse l) == (l :: [Int])
+  it "combinations of a single element is empty" $
+    combinations [1] `shouldBe` []
 
-  xit "solve the puzzle" $ do
-     input <- T.readFile "resources/input3"
-     logic input `shouldBe` Answer
+  it "combinations of a list of two  elements is the pair of elements" $
+    combinations [1, 2] `shouldBe` [(1, 2)]
+
+  it "expand all combinations, in the order of the initial list" $
+    combinations [1, 2, 3] `shouldBe` [(1, 2), (1, 3), (2, 3)]
+
+  it "expand all combinations, in the order of the initial list" $
+    combinations [1, 2, 3, 4, 5] `shouldBe` [(1, 2), (1, 3), (1, 4), (1, 5), (2, 3), (2, 4), (2, 5), (3, 4), (3, 5), (4, 5)]
+
+  it "unique combinations with different elements" $
+    uniqueCombinations [1, 2, 3, 1, 1] `shouldBe` [(1, 2), (1, 3), (1, 1), (2, 3), (2, 1), (3, 1)]
+
+  it "unique combinations in case of repeated digits" $
+    uniqueCombinations [1, 1, 1, 1] `shouldBe` [(1, 1)]
+
+  prop "combinations and uniqueCombinations produce the same list highest pair" $
+    1 `shouldBe` 1
+
+  it "highest joltage pair" $
+    highestJoltageBatteryPair powerBank1 `shouldBe` Just ((Battery Nine, Battery Eight))
+
+  it "highest joltage of powerbank 1" $
+    highestJoltage powerBank1 `shouldBe` 98
+
+  it "highest joltage of powerbank 2" $
+    highestJoltage powerBank2 `shouldBe` 89
+
+  it "highest joltage of powerbank 3" $
+    highestJoltage powerBank3 `shouldBe` 78
+
+  it "highest joltage of powerbank 4" $
+    highestJoltage powerBank4 `shouldBe` 92
+  it "solve part 1" $
+    solvePart1 powerBanks `shouldBe` 357
