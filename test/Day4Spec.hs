617051133ec6fa8c90ca24b0545918e149c04b2e
Alessandro Candolini <alessandrocandolini@gmail.com>
2025-12-06 05:08:56 +0000

    Day 4 part 1, using the store comonad

--- test/Day4Spec.hs
+++ test/Day4Spec.hs
@@ -1,21 +1,215 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day4Spec where
 
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Day4
+import Day4 (accessibilityGrid)
+import NeatInterpolation (trimming)
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
+
+input :: T.Text
+input =
+  [trimming|
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+|]
+
+grid :: [[Maybe Cell]]
+grid =
+  [ [Nothing, Nothing, Just RollsOfPaper, Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Nothing]
+  , [Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Nothing, Just RollsOfPaper, Nothing, Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper]
+  , [Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Nothing, Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper]
+  , [Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Nothing, Nothing, Just RollsOfPaper, Nothing]
+  , [Just RollsOfPaper, Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper]
+  , [Nothing, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Nothing, Just RollsOfPaper]
+  , [Nothing, Just RollsOfPaper, Nothing, Just RollsOfPaper, Nothing, Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper]
+  , [Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper]
+  , [Nothing, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Nothing]
+  , [Just RollsOfPaper, Nothing, Just RollsOfPaper, Nothing, Just RollsOfPaper, Just RollsOfPaper, Just RollsOfPaper, Nothing, Just RollsOfPaper, Nothing]
+  ]
+
+renderedOutput :: T.Text
+renderedOutput =
+  [trimming|
+..xx.xx@x.
+x@@.@.@.@@
+@@@@@.x.@@
+@.@@@@..@.
+x@.@@@@.@x
+.@@@@@@@.@
+.@.@.@.@@@
+x.@@@.@@@@
+.@@@@@@@@.
+x.x.@@@.x.
+|]
+
+expectedOutput :: [[Maybe Accessibility]]
+expectedOutput =
+  [
+    [ Nothing
+    , Nothing
+    , Just Accessible
+    , Just Accessible
+    , Nothing
+    , Just Accessible
+    , Just Accessible
+    , Just Inaccessible
+    , Just Accessible
+    , Nothing
+    ]
+  ,
+    [ Just Accessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    ]
+  ,
+    [ Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Nothing
+    , Just Accessible
+    , Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    ]
+  ,
+    [ Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Nothing
+    , Nothing
+    , Just Inaccessible
+    , Nothing
+    ]
+  ,
+    [ Just Accessible
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Just Accessible
+    ]
+  ,
+    [ Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    ]
+  ,
+    [ Nothing
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    ]
+  ,
+    [ Just Accessible
+    , Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    ]
+  ,
+    [ Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Nothing
+    ]
+  ,
+    [ Just Accessible
+    , Nothing
+    , Just Accessible
+    , Nothing
+    , Just Inaccessible
+    , Just Inaccessible
+    , Just Inaccessible
+    , Nothing
+    , Just Accessible
+    , Nothing
+    ]
+  ]
+
+displayAccessibility :: Maybe Accessibility -> Char
+displayAccessibility (Just Accessible) = 'x'
+displayAccessibility (Just Inaccessible) = '@'
+displayAccessibility Nothing = '.'
 
 spec :: Spec
 spec = describe "Day 4" $ do
-  it ""
-    $ 1
-    `shouldBe` 1
+  it "can parse input" $
+    parse input `shouldBe` Right grid
 
-  prop ""
-    $ \l -> reverse (reverse l) == (l :: [Int])
+  it "calculate bounds" $
+    let expectedBounds = Bounds 10 10
+        (bs, _) = accessibilityGrid 4 grid
+     in bs `shouldBe` expectedBounds
 
-  xit "solve the puzzle" $ do
-     input <- T.readFile "resources/input4"
-     logic input `shouldBe` Answer
+  it "calculate accessibility grid" $
+    let
+      (bs, g) = accessibilityGrid 4 grid
+      elements = toList bs g
+     in
+      elements `shouldBe` expectedOutput
+
+  it "print accessibility grid" $
+    let
+      (bs, g) = accessibilityGrid 4 grid
+      rows = unlines (fmap (fmap displayAccessibility) (toList bs g))
+     in
+      T.strip (T.pack rows) `shouldBe` renderedOutput
+
+  it "can solve part 1" $
+    solvePart1 grid `shouldBe` 13
+
+  it "solve both parts from input" $
+    solve input `shouldBe` Right (Solution 13 13)
