68841dd1e333b21219809fd1f0d4d24869d332c9
Alessandro Candolini <alessandrocandolini@gmail.com>
2025-12-08 16:09:16 +0000

    Day 6 part 1

--- test/Day6Spec.hs
+++ test/Day6Spec.hs
@@ -1,21 +1,62 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Day6Spec where
 
 import Day6
 import Test.Hspec
 import Test.Hspec.QuickCheck
-import Test.QuickCheck
 import qualified Data.Text as T
-import qualified Data.Text.IO as T
+import NeatInterpolation (trimming)
+import Text.Megaparsec (runParser)
+import SpecUtils (shouldBePretty)
+
+input :: T.Text
+input = [trimming|
+123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   +
+|]
+
+rows :: [[Integer]]
+rows =[[123, 328, 51, 64], [45,64,387,23], [6, 98, 215, 314]]
+
+operationsInput :: T.Text
+operationsInput = [trimming|
+*   +   *   +
+|]
+
+operations :: [Operation]
+operations = [Multiplication, Addition, Multiplication, Addition]
+
+parsedInput :: Input
+parsedInput = Input rows operations
 
 spec :: Spec
 spec = describe "Day 5" $ do
-  it ""
-    $ 1
-    `shouldBe` 1
+  it "parse operations" $
+    runParser operationsParser "input day 6" operationsInput `shouldBe` (Right operations)
+
+  it "rows parser" $
+    runParser rowsParser "input day 6" input `shouldBePretty` (Right rows)
+
+  it "parse full input" $
+    parse input `shouldBePretty` Right parsedInput
+
+  it "apply operation addition" $
+    applyOperation Addition [1,2,3,4] `shouldBe` 10
+
+  it "apply operation addition" $
+    applyOperation Multiplication [1,2,3,4] `shouldBe` 24
+
+  it "apply operations" $
+    applyOperations [(Addition, [1,2,3,4]), (Multiplication, [1,2,3,4])] `shouldBe` [10, 24]
+
+  it "solve part 1 " $
+    solvePart1 parsedInput `shouldBe` 4277556
+
+  it "Solve both parts from input" $
+    solve input `shouldBePretty` Right (Solution 4277556 4277556)
 
   prop ""
     $ \l -> reverse (reverse l) == (l :: [Int])
 
-  xit "solve the puzzle" $ do
-     input <- T.readFile "resources/input6"
-     logic input `shouldBe` Answer
